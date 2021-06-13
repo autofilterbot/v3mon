9ac49868de7be12f55e449d01ae52baf5ad8e708
autofilterbot <85702142+autofilterbot@users.noreply.github.com>
2021-06-13 11:07:42 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = "@SULTHANBOY10",
+                caption = f"<code>{file_size}{file_name}</code>",
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
